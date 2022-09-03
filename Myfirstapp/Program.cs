ec42c8d88f356ba5883c469355e8b6c9bdf3ca45
PetrikHunt <petrikhuntofficial@gmail.com>
2022-09-03 08:47:39 +0300

    create new app

---
+++ Myfirstapp/Program.cs
@@ -0,0 +1,14 @@
+﻿// //This is a comment to my code
+
+//rakenuds küsib kasutaja sisestada tema nime
+//rakendus tervitab kasutajat nimepidi
+
+Console.WriteLine("Enter your name:");
+string userName;
+
+userName = Console.ReadLine();
+
+Console.WriteLine("Hello " + userName + "!");  
+//string interpolation
+
+Console.WriteLine($"Hello, {userName}!");
